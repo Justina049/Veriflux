aea76e44b9eafeee0018b7d05be152e7d86fb09f
Justina049 <enenchejustina@gmail.com>
2025-06-03 03:14:40 +0100

    Added Recipient topbar component

--- src/veriflux_chain_frontend/src/Components/recipient/layout/RecipientTopBar.jsx
+++ src/veriflux_chain_frontend/src/Components/recipient/layout/RecipientTopBar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './RecipientTopBar.scss';
 
 const RecipientTopBar = () => {
   return (
-    <div>RecipientTopBar</div>
-  )
-}
+    <header className="recipient-topbar">
+      <div className="left-section">
+        <h2>Welcome, Recipient</h2>
+      </div>
+      <div className="right-section">
+        <button className="logout-btn">Logout</button>
+      </div>
+    </header>
+  );
+};
 
-export default RecipientTopBar
+export default RecipientTopBar;
